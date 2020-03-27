10026df6018f7a8587441f4f2914461af50da5d8
jay sarmi <esarmi21@gmail.com>
2020-03-26 21:42:25 -0700

    dsa drills

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import LinearSearch from "./LinearSearch";
+// import BinarySearch from "./BinarySearch"
+
+
+function App() {
+  return (
+    <div className="App">
+      <LinearSearch/>
+      {/* <BinarySearch/> */}
+    </div>
+  );
+}
+
+export default App;
